4a45ce0d229959a60a0cd90d5498e8a79d961b1c
zach <springerzman@gmail.com>
2026-02-09 22:20:00 -0800

    file upload test complete

--- pages/FileUploadPage.ts
+++ pages/FileUploadPage.ts
@@ -1,15 +1,38 @@
-import type { Page } from "@playwright/test";
+import type { Locator, Page } from "@playwright/test";
 
 // upload a file image and assert the file's name
 
 export class FileUploadPage {
   private readonly page: Page;
 
+  readonly numFilesText: Locator;
+  readonly uploadButton: Locator;
+  readonly files: Locator;
+
   constructor(page: Page) {
     this.page = page;
+    this.numFilesText = this.page.locator("#num-of-files");
+    this.uploadButton = this.page.locator(".btn-green-outline");
+    this.files = this.page.getByRole("figure");
   }
 
   async goto() {
     await this.page.goto("/apps/upload");
+  }
+
+  async uploadFiles(filePaths: string[]) {
+    const [fileChooser] = await Promise.all([
+      this.page.waitForEvent("filechooser"),
+      await this.uploadButton.click(),
+    ]);
+    await fileChooser.setFiles(filePaths);
+  }
+
+  getFileName(filePath: string) {
+    const split = filePath.split("/");
+    if (!split[split.length - 1]) {
+      throw new Error("File name not present in path");
+    }
+    return split[split.length - 1]!;
   }
 }
--- tests/file-upload.spec.ts
+++ tests/file-upload.spec.ts
@@ -1,8 +1,10 @@
-import { test as base } from "@playwright/test";
+import { test as base, expect } from "@playwright/test";
 import { FileUploadPage } from "../pages/FileUploadPage";
+import path from "path";
 
 type Fixture = {
   fu: FileUploadPage;
+  filePaths: string[];
 }
 
 const test = base.extend<Fixture>({
@@ -11,10 +13,39 @@ const test = base.extend<Fixture>({
     await fu.goto();
     await use(fu);
   },
+  filePaths: async ({ }, use) => {
+    const filePaths = [
+      path.join(process.cwd(), "public", "test-image1.jpg"),
+      path.join(process.cwd(), "public", "test-image2.jpg"),
+      path.join(process.cwd(), "public", "test-image3.jpg"),
+      path.join(process.cwd(), "public", "test-image4.jpg"),
+    ];
+    await use(filePaths);
+  },
 });
 
 test.describe("File Upload Page tests", () => {
-  test("Should show file name after upload", async ({ fu }) => {
+  test("Should show file name after 1 file uploaded", async ({ fu, filePaths }) => {
+    await expect(fu.uploadButton).toBeVisible();
+    await expect(fu.numFilesText).toHaveText("No File Selected");
 
+    const filePath = filePaths[0]!;
+    await fu.uploadFiles([filePath]);
+
+    await expect(fu.numFilesText).toHaveText("1 File Selected");
+    await expect(fu.files).toHaveText(fu.getFileName(filePath));
+  });
+
+  test("Should show file names after uploading multiple files", async ({ fu, filePaths }) => {
+    await expect(fu.uploadButton).toBeVisible();
+    await expect(fu.numFilesText).toHaveText("No File Selected");
+
+    await fu.uploadFiles(filePaths);
+
+    await expect(fu.files).toHaveCount(filePaths.length);
+    await expect(fu.numFilesText).toHaveText(`${filePaths.length} Files Selected`);
+    for (let i = 0; i < filePaths.length; i++) {
+      await expect(fu.files.nth(i)).toHaveText(fu.getFileName(filePaths[i]!))
+    }
   });
 }); 
