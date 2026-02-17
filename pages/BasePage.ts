0cc9098d36ffae99936e346c3708ff0bdc149769
zach <springerzman@gmail.com>
2026-02-17 00:20:56 -0800

    finished

--- pages/BasePage.ts
+++
@@ -1,9 +0,0 @@
-import { type Page } from "@playwright/test";
-
-export class BasePage {
-  constructor(protected page: Page) {}
-
-  async navigate(path: string) {
-    await this.page.goto(path);
-  }
-}
--- tests/fetch.spec.ts
+++ tests/fetch.spec.ts
@@ -3,7 +3,12 @@ import test, { expect } from "@playwright/test";
 
 test.describe("Fetch Page Tests", () => {
   test("wait for posts request and assert post count", async ({ page }) => {
-    await page.goto("/apps/fetch");
+    const responsePromise = page.waitForResponse("https://jsonplaceholder.typicode.com/posts");
+
+    await Promise.all([
+      page.goto("/apps/fetch"),
+      responsePromise,
+    ])
     expect(await page.locator(".icard").count()).toBeGreaterThan(50);
   });
 });
